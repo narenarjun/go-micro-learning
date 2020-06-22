fac4e6c48dc76f519fc704ec9b7349b13272b088
Narendran <narendev0610@gmail.com>
2020-06-22 14:20:30 +0530

    structured the app, graceful shutdown implemented with timeout

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/narenarjun/go-micro-learning/handlers"
 )
@@ -19,5 +22,38 @@ func main()  {
 	sm.Handle("/",hh)
 	sm.Handle("/goodluck",gh)
 
-	http.ListenAndServe(":8080",sm)
+	// ! creating a server
+	s := &http.Server{
+		Addr: ":8080",
+		Handler: sm,
+		IdleTimeout: 120*time.Second,
+		ReadTimeout: 1*time.Second,
+		WriteTimeout: 1*time.Second,
+	}
+
+	go func(){
+
+	 err :=	s.ListenAndServe()
+	 if err != nil{
+		 l.Fatal(err)
+	 }
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan,os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <- sigChan
+	l.Println("Recieved terminate, graceful shutdown",sig)
+
+	// graceful shutdown -- is really important.
+	/* 
+	! this would allow us to close any opened database or other server functions or other leaks to close
+	? before going into a abrubt shutdown
+	*/
+	// timoutcontext
+	tc,_ := context.WithTimeout(context.Background(),30 * time.Second)
+	 s.Shutdown(tc)
+
+	// http.ListenAndServe(":8080",sm)
 }
